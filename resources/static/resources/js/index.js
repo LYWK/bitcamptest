2276e3d79e3af2911b4e53e2e196e16f8e2d66ed
lyeman <lyeman@naver.com>
2019-06-12 14:04:50 +0900

    14:03

--- resources/static/resources/js/index.js
+++ resources/static/resources/js/index.js
@@ -1,22 +1,58 @@
-alert('자바스크립트 연결 성공 !!');
 
-var wrapper = document.querySelector('#wrapper');
-wrapper.innerHTML = '<h1>spa  시작</h1> '
-                   +'<div id = "target">삭제할 내용 </div>'
-                   +'<button id = "btn">지워버리자</button>'
-                   +'<button id = "btn2">추가해버리자</button>';
-var btn = document.querySelector('#btn');  //객체 생성 
-var target = document.getElementsById("target");
-btn.addEventListener('click',function(){
-    // wrapper.innerHTML = '<h1>spa  시작</h1> '+'<div>       </div>'+ '<button id = "btn">지워버리자</button>';
- //   document.getElementById('delete').innerHTML = '';
-   target.innerHTML = '';
 
-});
+var app = (function(){//app 에서  init 를 알수 없다. 
+    let init= function(){
+      login_form();
 
-var btn2 = document.querySelector('#btn2');
+    }
+    let login_form = function(){
+      var wrapper = document.querySelector('#wrapper');
+      wrapper.innerHTML = '<form action="/action_page.php">'
+      +'  First name:<br>'
+      +'  <input type="text" name="firstname" value="Mickey">'
+      +'  <br>'
+      +'  Last name:<br>'
+      +'  <input type="text" name="lastname" value="Mouse">'
+      +'  <br><br>'
+      +'  <input type="submit" value="Submit">'
+      +'</form> ';
+      var btn = document.querySelector('#btn');  //객체 생성 
+      var target = document.getElementById("target");
+      btn.addEventListener('click',function(){
+          // wrapper.innerHTML = '<h1>spa  시작</h1> '+'<div>       </div>'+ '<button id = "btn">지워버리자</button>';
+       //   document.getElementById('delete').innerHTML = '';
+         target.innerHTML = '';
+      
+      });
 
-btn2.addEventListener('click',function(){
-     var temp = document.createTextNode('새추가');
-      target.appendChild(temp);
-});
+      
+      var btn2 = document.querySelector('#btn2');
+      
+      btn2.addEventListener('click',function(){
+           var temp = document.createTextNode('새추가');
+            target.appendChild(temp);
+      });
+
+      var Join = document.querySelector('#Join');
+      Join.innerHTML = '<form>'
+      +'   ID:<br>'
+      +'  <input type="text" name="id">'
+      +'  <br>'
+      +'   Pass:<br>'
+      +'  <input type="text" name="pass">'
+      +'   Name:<br>'
+      +'  <input type="text" name="Name">'
+      +'   Ssn:<br>'
+      +'  <input type="text" name="Ssn">'
+      +'   Phone:<br>'
+      +'  <input type="text" name="Phone">'
+      +'</form>';
+    }
+
+    let Join_form = function(){
+
+    }
+    return {init : init};// closure  -app에 init로 property 등록 
+})();
+
+   
